8dcdbba26c6af468d72c4468ea2d232c4196d695
addy1205 <azadadnan113@gmail.com>
2024-12-03 18:41:11 +0530

    Continue

---
+++ src/component/Education.jsx
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react'
+import educationIcon from "../assets/educationIcon.png"
+import img10th from "../assets/10thImg.jpg"
+import img12th from "../assets/12thImg.jpg"
+import bcaImg from "../assets/bcaImg.jpg"
+
+
+const educationArray = [
+    {
+        qualification: "10th - Board",
+        year: "2017-2018",
+        grade: "Percentage - 85%",
+        institution: "Nepal Rawani Memorial H/S, Godhar",
+        image: img10th
+    },
+    {
+        qualification: "12th - Science",
+        year: "2019-2020",
+        grade: "Percentage - 77.4%",
+        institution: "Jharia Raj +2 High School, Jharia",
+        image: img12th
+    },
+    {
+        qualification: "Bachelor's of Computer Application",
+        year: "2021-2024",
+        grade: "CGPA - 7.62",
+        institution: "Guru Nanak College, Dhanbad",
+        image: bcaImg
+    },
+]
+
+function Education() {
+    const [forceRender, setForceRender] = useState(false);
+
+  useEffect(() => {
+    setForceRender(true); // Forces re-render after the component is mounted
+  }, []);
+
+    return (
+        <div id='Education' className={`w-full font-nunito mt-[30px] pt-[130px] `}>
+            <div className='text-center mx-auto px-[8%] '>
+                <div className='flex gap-2 md:gap-5 justify-center items-center mb-0'>
+                    <img className='w-6' src={educationIcon} alt="education icon" />
+                    <h1 className='text-xl sm:text-2xl md:text-3xl font-bold'>Education</h1>
+                </div>
+                <div className={`flex flex-wrap lg:flex-nowrap gap-5 my-14 ${forceRender ? 'opacity-100' : 'opacity-0'}`}>
+                    {educationArray.map((elm, index) => (
+                        <div key={index} className='educationCard w-[90%] flex flex-col gap-12 justify-between rounded bg-zinc-950 px-5 sm:px-10 py-8'  >
+                            <div  className='text-left'>
+                                <h2 className='text-lg text-[#c5ba22] font-semibold'>{elm.qualification}</h2>
+                                <h2 className='text-[13px] py-2 text-blue-500'>{elm.year}</h2>
+                                <h2 className='text-[13px] pb-2 text-green-500'>{elm.grade}</h2>
+                                <h2>{elm.institution}</h2>
+                            </div>
+                            {/* <img className='w-[40%] h-[100px] object-cover rounded' src={elm.image} alt="education image" loading="eager" /> */}
+ 
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Education
---
+++ src/component/Skills.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import htmlIcon from "../assets/html-5.png"
+import cssIcon from "../assets/css-3.png"
+import jsIcon from "../assets/js.png"
+import reactIcon from "../assets/reactjs.png"
+import python from "../assets/python.png"
+import java from "../assets/java.png"
+import tailwindIcon from "../assets/tailwind.png"
+import github from "../assets/github.png"
+import laptopIcon from "../assets/laptop.png"
+
+
+const skillsArray = [
+    {
+        image: htmlIcon,
+        name: "HTML"
+    },
+    {
+        image: cssIcon,
+        name: "CSS"
+    },
+    {
+        image: jsIcon,
+        name: "Javascript"
+    },
+    {
+        image: reactIcon,
+        name: "React Js"
+    },
+    {
+        image: python,
+        name: "Python"
+    },
+    {
+        image: java,
+        name: "Java"
+    },
+    {
+        image: tailwindIcon,
+        name: "Tailwind CSS"
+    },
+    {
+        image: github,
+        name: "GitHub"
+    },
+]
+
+function Skills() {
+
+    return (
+        <div id='Skills' className='w-full mt-[30px] pt-[130px] font-nunito'>
+            <div  className='text-center w-[80%] 2xl:w-[60%] mx-auto'>
+                <div className='flex gap-2 md:gap-5 justify-center items-center'>
+                    <img className='w-6' src={laptopIcon} alt="icon" />
+                    <h1 className='text-xl sm:text-2xl md:text-3xl font-bold'>Skills</h1>
+                </div>
+                <div className='skillsDiv py-12 flex flex-wrap gap-5 sm:gap-7 justify-center'>
+                    {
+                        skillsArray.map((elm, index) => (
+                            <div key={index} className='skillCard w-[130px] sm:w-[180px] bg-zinc-950 px-7 py-8 flex flex-col items-center rounded-md'>
+                                <img className='w-[30px] sm:w-[50px]' src={elm.image} alt="html icon" />
+                                <p className='pt-5 text-sm sm:text-md text-[#c5ba22] font-bold'>{elm.name}</p>
+                            </div>
+
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
